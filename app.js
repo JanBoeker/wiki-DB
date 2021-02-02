bbb74c0167ebb12ab7516c2da253083b3649e0db
Jan Böker <naj_b@gmx.de>
2021-02-02 21:57:01 +0100

    add get-, post- and delete route

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
 
+// DB setup
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -31,8 +32,68 @@ const articleSchema = {
   }
 };
 
-const Articel = new mongoose.model("Article", articleSchema);
+const Article = new mongoose.model("Article", articleSchema);
 
+// API
+// get all written articles
+app.get("/articles", function(req, res){
+  Article.find(function(err, foundArticles){
+    if (!err) {
+      if (foundArticles.length === 0) {
+        res.send("No articles found.")
+      } else {
+        res.send(foundArticles);
+      }
+
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// write a new article
+app.post("/articles", function(req, res) {
+
+  // Check if there is an article with the same title.
+  Article.findOne({title: req.query.title}, function(err, foundArticle) {
+    if (!err) {
+
+      // If there is no article with this tilte, insert the new article.
+      if (!foundArticle) {
+        const newArticle = new Article({
+          title: req.query.title,
+          content: req.query.content
+        });
+
+        newArticle.save(function(err) {
+          if (!err) {
+            res.send("You just successfully added a new article.");
+          } else {
+            res.send(err);
+          }
+        });
+      } else {
+        res.send("The article already exists.");
+      }
+    // Throw an error if it's not possible to read the DB.
+    } else {
+      res.send("An error occured while searching the DB.");
+    }
+  });
+});
+
+// Delete all existing article
+app.delete("/articles", function(req, res) {
+  Article.deleteMany(function(err) {
+    if (!err) {
+      res.send("Successfully deleted all articles.")
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// Setup server on Post 3000 --> localhost:3000
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
