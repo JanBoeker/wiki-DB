890b0af6a40d01ad4be3b8313f86aec3cd0ff81f
Jan Böker <naj_b@gmx.de>
2021-04-07 23:51:40 +0200

    final commit

--- app.js
+++ app.js
@@ -35,8 +35,12 @@ const articleSchema = {
 const Article = new mongoose.model("Article", articleSchema);
 
 // API
+
+///////////////////////////////////////////// all articles /////////////////////////////////////////////
+app.route("/articles")
+
 // get all written articles
-app.get("/articles", function(req, res){
+.get(function(req, res){
   Article.find(function(err, foundArticles){
     if (!err) {
       if (foundArticles.length === 0) {
@@ -49,11 +53,10 @@ app.get("/articles", function(req, res){
       res.send(err);
     }
   });
-});
+})
 
 // write a new article
-app.post("/articles", function(req, res) {
-
+.post(function(req, res) {
   // Check if there is an article with the same title.
   Article.findOne({title: req.query.title}, function(err, foundArticle) {
     if (!err) {
@@ -80,10 +83,10 @@ app.post("/articles", function(req, res) {
       res.send("An error occured while searching the DB.");
     }
   });
-});
+})
 
 // Delete all existing article
-app.delete("/articles", function(req, res) {
+.delete(function(req, res) {
   Article.deleteMany(function(err) {
     if (!err) {
       res.send("Successfully deleted all articles.")
@@ -92,6 +95,100 @@ app.delete("/articles", function(req, res) {
     }
   });
 });
+
+
+///////////////////////////////////////////// a specific article /////////////////////////////////////////////
+app.route("/articles/:articleTitle")
+
+//get a specific article
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle) {
+    if (!err) {
+
+      if (!foundArticle) {
+        res.send("An article with the title " + req.params.articleTitle + " does not exists.");
+      } else {
+        res.send(foundArticle);
+      }
+    // Throw an error if it's not possible to read the DB.
+    } else {
+      res.send("An error occured while searching the DB.");
+    }
+  });
+
+})
+
+//update a specific articles
+.put(function(req, res){
+
+  Article.findOneAndUpdate(
+    {title: req.params.articleTitle},
+    {title: req.query.title, content: req.query.content},
+    {overwrite: true},
+    function(err, article){
+      if(!err) {
+        if (article) {
+          res.send("Successfully replaced the article with the title: '"+ req.params.articleTitle + "'.'");
+        } else {
+          res.send("An article with the title '" + req.params.articleTitle + "' does not exists.");
+        }
+      } else {
+        res.send(err);
+      }
+  });
+
+})
+
+//update a specific articles without overwriting
+.patch(function(req, res){
+
+  Article.findOne(
+    {title: req.params.articleTitle},
+    function(err, foundArticle) {
+      if (!err) {
+        if (foundArticle) {
+          Article.updateOne(
+            {title: req.params.articleTitle},
+            {$set: req.query},
+            function(err) {
+              if (!err) {
+                  res.send("Successfully updated the article with the title: '"+ req.params.articleTitle + "'.'");
+              } else {
+                res.send(err);
+              }
+            }
+          );
+        } else {
+          res.send("An article with the title '" + req.params.articleTitle + "' does not exists.");
+        }
+      } else {
+        res.send(err);
+      }
+    }
+  );
+
+})
+
+// delete a specific article
+.delete(function(req, res){
+
+  Article.findOneAndDelete(
+    {title: req.params.articleTitle},
+    function(err, article) {
+    if (!err) {
+      if (article) {
+        res.send("Successfully delete the article with the tile: '" + req.params.articleTitle + "'.");
+      } else {
+        res.send("An article with the title '" + req.params.articleTitle + "' does not exists.");
+      }
+    } else {
+      res.send(err);
+    }
+  });
+
+});
+
 
 // Setup server on Post 3000 --> localhost:3000
 app.listen(3000, function() {
